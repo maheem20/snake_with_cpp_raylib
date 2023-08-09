fa61dae371d8b4b906faa599d09704ad89b52465
maheem_20 <mwistanbuly@gmail.com>
2023-08-09 23:37:46 +0200

    create grid

--- main.cpp
+++ main.cpp
@@ -6,11 +6,14 @@ using namespace std;
 Color green = {173, 204, 96, 255};
 Color darkGreen = {43, 51, 24, 255};
 
+int cellSize = 30;
+int cellCount = 25;
+
 int main()
 {
 
     cout << "Starting game..." << endl;
-    InitWindow(750, 750, "Retro Snake");
+    InitWindow(cellSize * cellCount, cellSize * cellCount, "Retro Snake");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
