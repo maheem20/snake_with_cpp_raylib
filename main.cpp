0b104cf66af6bd2b46c1a27887d60ad0e01a093c
maheem_20 <mwistanbuly@gmail.com>
2023-08-09 23:44:25 +0200

    define Draw method

--- main.cpp
+++ main.cpp
@@ -11,8 +11,13 @@ int cellCount = 25;
 
 class Food
 {
-    public:
-        Vector2 position = {5, 6};
+public:
+    Vector2 position = {5, 6};
+
+    void Draw()
+    {
+        DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+    }
 };
 
 int main()
