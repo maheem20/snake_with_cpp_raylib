86c2b41ef611d5ddca5aaae96d49eabf7acdd2cb
maheem_20 <mwistanbuly@gmail.com>
2023-08-12 23:51:52 +0200

    check if current element is equal to given element

--- main.cpp
+++ main.cpp
@@ -17,7 +17,12 @@ bool ElementInDeque(Vector2 element, deque<Vector2> deque)
 {
     for (unsigned int i = 0; i < deque.size(); i++)
     {
+        if (Vector2Equals(element, deque[i]))
+        {
+            return true;
+        }
     }
+    return false;
 }
 
 bool eventTriggered(double interval)
