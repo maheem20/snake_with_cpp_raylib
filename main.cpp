919416f4ae5213330fdfbd31bd78a410721a8a29
maheem_20 <mwistanbuly@gmail.com>
2023-08-12 17:23:34 +0200

    prevent changing to opposite direction

--- main.cpp
+++ main.cpp
@@ -99,19 +99,19 @@ int main()
             snake.Update();
         }
 
-        if (IsKeyPressed(KEY_UP))
+        if (IsKeyPressed(KEY_UP) && snake.direction.y != 1)
         {
             snake.direction = {0, -1};
         }
-        if (IsKeyPressed(KEY_DOWN))
+        if (IsKeyPressed(KEY_DOWN) && snake.direction.y != -1)
         {
             snake.direction = {0, 1};
         }
-        if (IsKeyPressed(KEY_LEFT))
+        if (IsKeyPressed(KEY_LEFT) && snake.direction.x != 1)
         {
             snake.direction = {-1, 0};
         }
-        if (IsKeyPressed(KEY_RIGHT))
+        if (IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1)
         {
             snake.direction = {1, 0};
         }
