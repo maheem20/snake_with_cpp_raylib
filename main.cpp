401c4261f079681654cdaa20d36315d330eaf358
maheem_20 <mwistanbuly@gmail.com>
2023-08-12 20:04:16 +0200

    make changes after creating Game class

--- main.cpp
+++ main.cpp
@@ -112,24 +112,24 @@ int main()
 
         if (eventTriggered(0.2))
         {
-            snake.Update();
+            game.Update();
         }
 
-        if (IsKeyPressed(KEY_UP) && snake.direction.y != 1)
+        if (IsKeyPressed(KEY_UP) && game.snake.direction.y != 1)
         {
-            snake.direction = {0, -1};
+            game.snake.direction = {0, -1};
         }
-        if (IsKeyPressed(KEY_DOWN) && snake.direction.y != -1)
+        if (IsKeyPressed(KEY_DOWN) && game.snake.direction.y != -1)
         {
-            snake.direction = {0, 1};
+            game.snake.direction = {0, 1};
         }
-        if (IsKeyPressed(KEY_LEFT) && snake.direction.x != 1)
+        if (IsKeyPressed(KEY_LEFT) && game.snake.direction.x != 1)
         {
-            snake.direction = {-1, 0};
+            game.snake.direction = {-1, 0};
         }
-        if (IsKeyPressed(KEY_RIGHT) && snake.direction.x != -1)
+        if (IsKeyPressed(KEY_RIGHT) && game.snake.direction.x != -1)
         {
-            snake.direction = {1, 0};
+            game.snake.direction = {1, 0};
         }
 
         ClearBackground(green);
