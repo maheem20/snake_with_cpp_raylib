866bd2d41594f2cf271a1af9609cc35fcb8ce9d1
maheem_20 <mwistanbuly@gmail.com>
2023-08-14 23:45:15 +0200

    draw score on screen

--- main.cpp
+++ main.cpp
@@ -225,6 +225,7 @@ int main()
 
         ClearBackground(green);
         DrawRectangleLinesEx(Rectangle{(float)offset - 5, (float)offset - 5, (float)cellSize * cellCount + 10, (float)cellSize * cellCount + 10}, 5, darkGreen);
+        DrawText(TextFormat("%i", game.score), offset - 5, offset + cellSize * cellCount + 10, 40, darkGreen);
         DrawText("Retro Snake", offset - 5, 20, 40, darkGreen);
         game.Draw();
 
